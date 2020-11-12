8f07ceb0ff711156d75dab8bbff186a35495ffbc
Ryan Price <ryan.c.price@hotmail.com>
2020-11-12 17:07:45 -0500

    Added an array for correct answers so that there can be more than one

--- rss-client/src/app/Quiz/admin/add-quiz/add-quiz.component.ts
+++ rss-client/src/app/Quiz/admin/add-quiz/add-quiz.component.ts
@@ -145,6 +145,7 @@ export class AddQuizComponent implements OnInit {
         option3: null,
         option4: null,
         option5: null,
+        correctAnswers: [0],
         quiz: {},
       };
     } else {
@@ -162,6 +163,7 @@ export class AddQuizComponent implements OnInit {
               question: result.value.question,
               questionValue: result.value.questionValue,
               correctAnswer: result.value.correctAnswer,
+              correctAnswers: this.focusedQuestion.correctAnswers,
               correctAnswerNumber: result.value.correctAnswerNumber,
             };    
             // Adds only options with not null values       
@@ -213,11 +215,30 @@ export class AddQuizComponent implements OnInit {
   /**updateCorrect
   * @param correctNumber
   * Takes the number of Correct Answers inputted and updates the number of inputs that can take.
+  * Also updating the array of Correct Answers to have the correct size
   */
- updateCorrect(correctNumber: number) {
+  updateCorrect(correctNumber: number) {
   //Creates the looping array with the size of the correctAnswerNumber
-  this.looper = this.foo.slice(0, correctNumber)
+    this.looper = this.foo.slice(0, correctNumber)
+    if (this.looper.length > this.focusedQuestion.correctAnswers.length) {
+      let k = this.looper.length - this.focusedQuestion.correctAnswers.length;
+      for (let i = 0; i < k; i++) {
+        this.focusedQuestion.correctAnswers.push(0);
+      }
+    }
+    else {
+      let temp = this.focusedQuestion.correctAnswers.slice(0, correctNumber);
+      this.focusedQuestion.correctAnswers = temp;
+    }
   
+  }
+  /**updateCorrectArray
+   * @param correct
+   * @param index
+   * Inputs the correct answer into the CorrectAnswerArray at the listed index
+   */
+  updateCorrectArray(correct: number, index: number) {
+    this.focusedQuestion.correctAnswers[index] = correct;
   }
 /**
  * getDismissReason()
